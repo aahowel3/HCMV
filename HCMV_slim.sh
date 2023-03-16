be1897fb5ef21de0dee6c1d160c99f4e511547c1
Abigail Howell <aahowel3@asu.edu>
2023-03-16 14:57:11 -0700

    Add files via upload

--- HCMV_slim.sh
+++ HCMV_slim.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -N 1            # number of nodes
 #SBATCH -n 1            # number of "tasks" (default: 1 core per task)
-#SBATCH -t 2-00:10:00   # time in d-hh:mm:ss
+#SBATCH -t 3-00:10:00   # time in d-hh:mm:ss
 #SBATCH -p general      # partition
 #SBATCH --mem=16000mb
 #SBATCH -q public       # QOS
@@ -22,12 +22,14 @@
 #python sc2-full_stats_forkAH.py urine.100 23500
 
 
-while IFS="," read -r col1 col2 col3 col4 col5 col6 col7 col8 col9 
+while IFS="," read -r col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11
 	do
-	slim -d DFE="$col1" -d f0="$col2" -d f1="$col3" -d f2="$col4" -d f3="$col5" -d recomb="$col6" -d mu="$col7" -d progeny="$col8" -d gr="$col9" HCMV_burnin_final.slim
-done < params_burnin_tester.txt
+	slim -d DFE="$col1" -d f0="$col2" -d f1="$col3" -d f2="$col4" -d f3="$col5" -d recombrate="$col6" -d recomb="$col7" -d murate="$col8" -d mu="$col9" -d progeny="$col10" -d gr="$col11" HCMV_burnin_final.slim
+done < xaa
 
-#while IFS="," read -r 1 2 3 4 5 6 7 8 9 10
+#should be params_burnin.txt but you're splitting it into xaa,xab etc with split --lines=72 params_burnin.txt
+
+#while IFS="," read -r col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11 col12
 #        do
-#        slim -d DFE="$1" f0="$2" f1="$3" f2="$4" f3="$5" recomb="$6" mu="$7" progeny="$8" burnin="$9" gr="$10" HCMV_congenital_final.slim
-#done <(tail -n +2 params_congenital_tester.txt)
+#        slim -d DFE="$col1" -d f0="$col2" -d f1="$col3" -d f2="$col4" -d f3="$col5" -d recombrate="$col6" -d recomb="$col7" -d murate="$col8" -d mu="$col9" -d progeny="$col10" -d burnin="$col11" -d gr="$col12" HCMV_congenital_final.slim 
+#done < params_congenital_tester.txt
--- var_mu_recomb_rate.R
+++ var_mu_recomb_rate.R
@@ -8,15 +8,15 @@ df1<-data.frame(pos,recomb_rate)
 mu_rate=runif(length(pos), min = 2e-8, max = 2e-6)
 df2<-data.frame(pos,mu_rate)
 
-write.csv(df1, "recomb.var.txt",row.names = FALSE, quote=FALSE)
-write.csv(df2, "mu.var.txt",row.names = FALSE, quote=FALSE)
+write.table(df1, "recomb.var.txt",row.names = FALSE, quote=FALSE,sep="\t",col.names=FALSE)
+write.table(df2, "mu.var.txt",row.names = FALSE, quote=FALSE,sep="\t",col.names=FALSE)
 
 
 #create params file for slims
 DFE = c("DFE1,0.5,0.24,0.12,0.13","DFE2,0.1,0.7,0.1,0.1",
         "DFE3,0.1,0.1,0.1,0.7")
-recomb = c(9.8e-6,9.8e-7,9.8e-8,"var")
-mu = c(2e-6,2e-7,2e-8,"var")
+recomb = c("fix,9.8e-6","fix,9.8e-7","fix,9.8e-8","var,0")
+mu = c("fix,2e-6","fix,2e-7","fix,2e-8","var,0")
 progeny = c(0.01,0.07,0.1)
 burnin= c("with","without")
 gr= c(0.38,1)
@@ -29,5 +29,14 @@ df=within(df, Var1<-data.frame(do.call('rbind', strsplit(as.character(Var1), ','
 df_burnin=within(df_burnin, Var1<-data.frame(do.call('rbind', strsplit(as.character(Var1), ',', fixed=TRUE))))
 
 
-write.csv(df, "params_congenital.txt",row.names = FALSE, quote=FALSE)
-write.csv(df_burnin, "params_burnin.txt",row.names = FALSE, quote=FALSE)
+df=within(df, Var2<-data.frame(do.call('rbind', strsplit(as.character(Var2), ',', fixed=TRUE))))
+df_burnin=within(df_burnin, Var2<-data.frame(do.call('rbind', strsplit(as.character(Var2), ',', fixed=TRUE))))
+
+
+df=within(df, Var3<-data.frame(do.call('rbind', strsplit(as.character(Var3), ',', fixed=TRUE))))
+df_burnin=within(df_burnin, Var3<-data.frame(do.call('rbind', strsplit(as.character(Var3), ',', fixed=TRUE))))
+
+write.csv(df, "params_congenital.txt",row.names = FALSE,quote=c(1,6,8,11))
+write.csv(df_burnin, "params_burnin.txt",row.names = FALSE, quote=c(1,6,8))
+
+
